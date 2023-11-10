036fe8e6b2a6cd45cb5b8032b59542025f6000f4
Jacob Amedie <jacob.amedie@gmail.com>
2023-11-10 10:04:48 -0800

    latest commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,20 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.android.gms:play-services-maps:18.2.0")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation(platform("com.google.firebase:firebase-bom:32.4.0"))
+    implementation("com.google.firebase:firebase-analytics-ktx")
+    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-crashlytics-ktx")
+    implementation("com.google.firebase:firebase-database-ktx")
 }
--- app/src/main/java/com/example/loginappinclass/MainActivity2.kt
+++ app/src/main/java/com/example/loginappinclass/MainActivity2.kt
@@ -2,46 +2,88 @@ package com.example.loginappinclass
 
 //import YelpAdapter
 import YelpAdapter
+import android.location.Address
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
 class MainActivity2 : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
+    private lateinit var myYelpText: EditText
+    private lateinit var addNewYelpBtn: FloatingActionButton
+    private lateinit var firebaseDatabase: FirebaseDatabase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
         recyclerView=findViewById(R.id.recyclerView)
+        myYelpText = findViewById(R.id.yelpText)
+        addNewYelpBtn = findViewById(R.id.floatingActionButton)
+        firebaseDatabase=FirebaseDatabase.getInstance()
 
-        val location:String = intent.getStringExtra("location").toString()
-        Log.d("MainActivity2", "Location is $location")
+//        val location:String = intent.getStringExtra("location").toString()
+        Log.d("MainActivity2", "Location is nnnn")
+//        Log.d("MainActivity2", "Location is $location")
 //        title = location
-        setTitle(location)
+//        setTitle(location)
 
+        val address = if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.TIRAMISU) {
+            intent.getParcelableExtra("address", Address::class.java)!!
+        }else {
+            intent.getParcelableExtra<Address>("address")!!
+        }
+/*     val yelpManager = YelpManager()
+        val gwuLat = address.latitude
+        val gwuLong = address.longitude
+        setTitle(address.adminArea)
+        val apiKey = getString(R.string.yel_api_key)
+        var yelps = listOf<YelpBusiness>()
+
+        //Coroutines
+        CoroutineScope(IO).launch {
+            yelps = yelpManager.retrieveYelps(gwuLat,gwuLong,apiKey)
+        }
+
+        withContext(Main){
+            val adapter = YelpAdapter(yelps)
+            recyclerView.adapter = adapter
+            recyclerView.layoutManager = LinearLayoutManager(this@MainActivity2)
+        }
+}
         val yelps = getFakeYelpData()
         val adapter=YelpAdapter(yelps)
         recyclerView.adapter=adapter
         recyclerView.layoutManager=LinearLayoutManager(this)
 
-    }
 
     private fun getFakeYelpData(): List<YelpBusiness>{
         return listOf(
-            YelpBusiness("Panera", "Breakfast", "4 star", "https://..."),
-            YelpBusiness("WingsToGo", "Comfort", "5 star" , "https://..."),
-            YelpBusiness("Log Cabin", "Seafood", "4 star", "https://..."),
-            YelpBusiness("Dunkin Donut", "Breakfast", "3 star", "https://..."),
-            YelpBusiness("Starbucks", "Coffee", "3 star", "https://..."),
-            YelpBusiness("Panera", "Breakfast", "3 star", "https://..."),
-            YelpBusiness("Panera", "Breakfast", "3 star" , "https://..."),
-            YelpBusiness("Panera", "Breakfast", "2 star" , "https://..."),
-            YelpBusiness("Panera", "Breakfast", "5 star", "https://..."),
-//            YelpBusiness("Subway","Dinner",1.2,"https://...")
-            YelpBusiness("Subway","Dinner","1.2","https://...")
+//            YelpBusiness("Panera", "Breakfast", "4 star", "https://..."),
+//            YelpBusiness("WingsToGo", "Comfort", "5 star" , "https://..."),
+//            YelpBusiness("Log Cabin", "Seafood", "4 star", "https://..."),
+//            YelpBusiness("Dunkin Donut", "Breakfast", "3 star", "https://..."),
+//            YelpBusiness("Starbucks", "Coffee", "3 star", "https://..."),
+//            YelpBusiness("Panera", "Breakfast", "3 star", "https://..."),
+//            YelpBusiness("Panera", "Breakfast", "3 star" , "https://..."),
+//            YelpBusiness("Panera", "Breakfast", "2 star" , "https://..."),
+//            YelpBusiness("Panera", "Breakfast", "5 star", "https://..."),
+////            YelpBusiness("Subway","Dinner",1.2,"https://...")
+//            YelpBusiness("Subway","Dinner","1.2","https://...")
         )
+    }
+    */
     }
 }
